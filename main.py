ac0f3cddd6359a0995655e8c51b924aa68305456
parkhs21 <2001phs92@gmail.com>
2024-03-11 16:30:36 +0900

    feat: test main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("HI")
+import os
+
+print()
+print(os.getcwd())
+print(file_list := os.listdir())
+
+for file in file_list:
+    if file.endswith(".py"):
+        print()
+        print(open(file).read())
