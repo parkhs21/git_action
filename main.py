a2f332d099250059d25c50db39e027d02395a442
parkhs21 <2001phs92@gmail.com>
2024-03-14 15:52:51 +0900

    feat: set environ variable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import dotenv
+import os
 from langchain_openai import ChatOpenAI
 
 dotenv.load_dotenv()
@@ -6,4 +7,5 @@ dotenv.load_dotenv()
 llm = ChatOpenAI()
 chat = llm.invoke("Do you know what TmaxSoft is? Answers should be in Korean.")
 
+os.environ["GITHUB_OUTPUT"]["result"] = chat
 print(chat)
