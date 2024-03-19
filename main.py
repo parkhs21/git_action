4f2f207d71916e3976b011670c1777ef948bd88a
parkhs21 <2001phs92@gmail.com>
2024-03-19 16:57:51 +0900

    fix: bash command

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ print(os.environ["EVENT_TYPE"])
 if os.environ["EVENT_TYPE"] == "issue_comment":
     chat = llm.invoke(os.environ["COMMENT"])
     chat = chat.content
-    os.system("::set-output name=result::" + chat)
+    command = '"::set-output name=result::' + chat + '"'
+    os.system('echo ' + command)
 else:
     chat = "Test Answer about PR."
 
