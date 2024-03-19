aa30d927521d367db6b6a6031749a9ba17d75510
parkhs21 <2001phs92@gmail.com>
2024-03-19 15:06:44 +0900

    feat: test env code

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from langchain_openai import ChatOpenAI
 
 # dotenv.load_dotenv()
 
+print("TEST")
+print(os.environ("COMMENT"))
+print("TEST")
+
 llm = ChatOpenAI()
 chat = llm.invoke("Do you know what TmaxSoft is? Answers should be in Korean.")
 # chat = "Test Answer."
