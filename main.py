295df2895e75fdb30ab7f6697fa0e29e9a230a90
parkhs21 <2001phs92@gmail.com>
2024-03-19 16:53:05 +0900

    feat: comment result to output

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-import dotenv
 import os
 from langchain_openai import ChatOpenAI
 
-# dotenv.load_dotenv()
-
-# llm = ChatOpenAI()
+llm = ChatOpenAI()
 # chat = llm.invoke("Do you know what TmaxSoft is? Answers should be in Korean.")
 
 print(os.environ["EVENT_TYPE"])
 
 if os.environ["EVENT_TYPE"] == "issue_comment":
-    chat = "Test Answer about Comment."
+    chat = llm.invoke(os.environ["COMMENT"])
+    chat = chat.content
+    os.system("::set-output name=result::" + chat)
 else:
     chat = "Test Answer about PR."
 
