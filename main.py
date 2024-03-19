b0a5b20ab4bfe0281c9ffab562bf711e097b024c
parkhs21 <2001phs92@gmail.com>
2024-03-19 14:22:27 +0900

    feat: test all basic login

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import dotenv
 import os
 from langchain_openai import ChatOpenAI
 
-dotenv.load_dotenv()
+# dotenv.load_dotenv()
 
-# llm = ChatOpenAI()
-# chat = llm.invoke("Do you know what TmaxSoft is? Answers should be in Korean.")
-chat = "Test Answer."
+llm = ChatOpenAI()
+chat = llm.invoke("Do you know what TmaxSoft is? Answers should be in Korean.")
+# chat = "Test Answer."
 
-with open("test.adoc", "w") as file:
+with open("answer.adoc", "w") as file:
     file.write(chat)
