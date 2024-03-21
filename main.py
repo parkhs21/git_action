9414a0d434e900c13141570bd7ae0cce15dd25aa
parkhs21 <2001phs92@gmail.com>
2024-03-21 14:00:32 +0900

    feat: add main llm logic

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import os
 from langchain_openai import ChatOpenAI
 
-llm = ChatOpenAI()
-# chat = llm.invoke("Do you know what TmaxSoft is? Answers should be in Korean.")
 
-print(os.environ["EVENT_TYPE"])
+llm = ChatOpenAI()
+
+
+prompt = lambda code: f'''
+Our purpose is to create a user manual for the entire program.
+
+In the meantime, let's create a table of contents. The table of contents is the most important bone of the manual.
+Since this manual is for the user, not the developer, it should be organized around the screens that will be configured.
+
+Let's create a large table of contents with the following code.
+Let’s think step by step.
+
+##### Below Code #####
+{code}
+'''.strip()
+
+
 
 if os.environ["EVENT_TYPE"] == "issue_comment":
     chat = llm.invoke(os.environ["COMMENT"])
-    chat = chat.content
-    command = '"::set-output name=result::' + chat + '"'
-    os.system('echo ' + command)
+    
 else:
-    chat = "Test Answer about PR."
+    with open("./local_test/src/org/yccheok/jstock/gui/OptionsJPanel.java") as file:
+        content = file.read()
+    chat = llm.invoke(prompt(content))
+
+
+
+command = '"::set-output name=result::' + chat + '"'
+os.system('echo ' + command)
+
 
 print(chat)
 with open("answer.adoc", "w") as file:
-    file.write(chat)
+    file.write(chat.content)
